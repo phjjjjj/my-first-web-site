9526a783d895d7e3a4bde930934ec5bad01e9f2b
phjjjjj <berryjiny@naver.com>
2023-05-22 15:26:51 +0900

    w12 수정

--- w12/script.js
+++ w12/script.js
@@ -21,10 +21,24 @@ function nightDayHandler(self){
           target.style.color = 'black';
           self.value = 'night';
           document.querySelector('#night_day_2').value = 'night';
+
+          var alist = document.querySelectorAll('a') ;
+          var i = 0;
+          while(i < alist.length){
+          alist[i].style.color = 'blue';
+          i = i + 1;
+          }
         } else {
           target.style.backgroundColor = 'black';
           target.style.color = 'white';
           self.value = 'day';
           document.querySelector('#night_day_2').value = 'day';
+
+          var alist = document.querySelectorAll('a') ;
+          var i = 0;
+          while(i < alist.length){
+          alist[i].style.color = 'powderblue';
+          i = i + 1;
+          }
         }
 }
